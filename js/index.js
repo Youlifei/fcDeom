aee8404f2ca076f59dc7b8b907aa2a562edcf1f4
You lifei <you@qq.com>
2019-02-21 12:00:55 +0800

    第二次修改

--- js/index.js
+++ js/index.js
@@ -3,3 +3,14 @@ $(".nav a").mouseenter(function(){
 }).mouseout(function(){
 	$(this).css("color","#fff")
 })
+//手风琴
+$(".ilistmw").mouseover(function(){
+	$(this).css({"height":130,"background":"#065706","border":0})
+}).mouseout(function(){
+	$(this).css({"height":79,"background":"#012701","border-bottom":"solid 1px #fff"})
+})
+$(".ilistmw a").mouseover(function(){
+	$(this).css("color","#e2b000")
+}).mouseout(function(){
+	$(this).css("color","#fff")
+})
